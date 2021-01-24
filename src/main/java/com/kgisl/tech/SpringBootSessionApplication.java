af3a4dac719b4bb98270dd03d72bc5bbd8a2d7c7
Tina P Ashok <tina@kgisl.com>
2021-01-24 23:22:04 +0800

    adding source code

---
+++ src/main/java/com/kgisl/tech/SpringBootSessionApplication.java
@@ -0,0 +1,14 @@
+package com.kgisl.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionApplication.class, args);
+	}
+	
+	
+}
